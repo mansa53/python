f3b19db562284d2289a1a3cde16fdb05e51d8e8c
mansa53 <31716134+mansa53@users.noreply.github.com>
2018-08-08 20:10:50 +0530

    Add files via upload

---
+++ NLTK-3----Natural-Language-Processing-with-Python-series-master/nltkvid19.3.py
@@ -0,0 +1,4 @@
+import sentiment_mod as s
+
+print(s.sentiment("This movie is okay.But the dialogues were awesome. Acting was horrible"))
+print(s.sentiment("This movie was utter junk. There were absolutely 0 pythons. I don't see what the point was at all. Horrible movie, 0/10"))
